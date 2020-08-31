873722838a136efc483e20b3e20a46cc8b30ea07
tiger1997715 <wolf970715@gmail.com>
2020-08-31 20:49:29 +0800

    2020.08.31 第二版

--- PopRecorder.py
+++ PopRecorder.py
@@ -17,34 +17,25 @@ gymPopXZ = XZPop[0]
 
 luzhouURL = "https://lzcsc.cyc.org.tw/api"
 getLZ = requests.get(luzhouURL)
-soupLZ = str(BeautifulSoup(getLZ.text ,'html.parser'))
-LZPop = eval(soupLZ)
-poolPopLZ = LZPop["swim"][0]
-gymPopLZ = LZPop["gym"][0]
+soupLZ = eval(str(BeautifulSoup(getLZ.text ,'html.parser')))
+poolPopLZ = soupLZ["swim"][0]
+gymPopLZ = soupLZ["gym"][0]
 
 
 sanchongURL = "http://www.scsports.com.tw/proxy1.php"
 getSC = requests.get(sanchongURL)
-soupSC = str(BeautifulSoup(getSC.text ,'html.parser'))
-SCPop = eval(soupSC)
-poolPopSC = SCPop["swim"][0]
-gymPopSC = SCPop["gym"][0]
+soupSC = eval(str(BeautifulSoup(getSC.text ,'html.parser')))
+poolPopSC = soupSC["swim"][0]
+gymPopSC = soupSC["gym"][0]
 
 
-taipeiURL = "http://booking.tpsc.sporetrofit.com/Home/LocationPeopleNum"
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless') # 啟動無頭模式
-chrome_options.add_argument('--disable-gpu') # windowsd必須加入此行
-chrome_options.add_argument('log-level=2')
-Options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-webdriver_path = '.\\chromedriver.exe'
-driver = webdriver.Chrome(executable_path = webdriver_path ,options = chrome_options)
-driver.get(taipeiURL) #前往這個網址
-poolPopDA = driver.find_element_by_xpath("//form[@id = 'form1']/div[@class = 'page']/div[@id = 'main']/div[@class = 'wrapper']/div[@class = 'shell']/div[@class = 'main']/div[@class = 'row']/div[@id = 'EachLidPeopleNumDiv']/div[@class = 'col-md-6']/div[@class = 'tra']/div[@class = 'tra-body']/div[@class = 'tra-space']/p/span[@id = 'CurSwPNum_DASC']").text
-gymPopDA = driver.find_element_by_xpath("//form[@id = 'form1']/div[@class = 'page']/div[@id = 'main']/div[@class = 'wrapper']/div[@class = 'shell']/div[@class = 'main']/div[@class = 'row']/div[@id = 'EachLidPeopleNumDiv']/div[@class = 'col-md-6']/div[@class = 'tra']/div[@class = 'tra-body']/div[@class = 'tra-space']/p/span[@id = 'CurGymPNum_DASC']").text
-poolPopDT = driver.find_element_by_xpath("//form[@id = 'form1']/div[@class = 'page']/div[@id = 'main']/div[@class = 'wrapper']/div[@class = 'shell']/div[@class = 'main']/div[@class = 'row']/div[@id = 'EachLidPeopleNumDiv']/div[@class = 'col-md-6']/div[@class = 'tra']/div[@class = 'tra-body']/div[@class = 'tra-space']/p/span[@id = 'CurSwPNum_DTSC']").text
-gymPopDT = driver.find_element_by_xpath("//form[@id = 'form1']/div[@class = 'page']/div[@id = 'main']/div[@class = 'wrapper']/div[@class = 'shell']/div[@class = 'main']/div[@class = 'row']/div[@id = 'EachLidPeopleNumDiv']/div[@class = 'col-md-6']/div[@class = 'tra']/div[@class = 'tra-body']/div[@class = 'tra-space']/p/span[@id = 'CurGymPNum_DTSC']").text
-driver.quit()
+taipeiURL = "http://booking.tpsc.sporetrofit.com/Home/loadLocationPeopleNum"
+getTP = requests.post(taipeiURL)
+soupTP = eval(str(BeautifulSoup(getTP.text ,'html.parser')))
+poolPopDA = soupTP['locationPeopleNums'][1]['swPeopleNum']
+gymPopDA = soupTP['locationPeopleNums'][1]['gymPeopleNum']
+poolPopDT = soupTP['locationPeopleNums'][2]['swPeopleNum']
+gymPopDT = soupTP['locationPeopleNums'][2]['gymPeopleNum']
 
 
 savefile = open(".\\運動人流記錄檔" + fileNameTime + ".txt","a+")
--- PopRecorder.spec
+++ PopRecorder.spec
@@ -3,8 +3,8 @@
 block_cipher = None
 
 
-a = Analysis(['poprecorder.py'],
-             pathex=['C:\\Users\\sam19\\Desktop\\人流紀錄'],
+a = Analysis(['PopRecorder.py'],
+             pathex=['D:\\github\\PopRecorder'],
              binaries=[],
              datas=[],
              hiddenimports=[],
@@ -21,12 +21,12 @@ exe = EXE(pyz,
           a.scripts,
           [],
           exclude_binaries=True,
-          name='poprecorder',
+          name='PopRecorder',
           debug=False,
           bootloader_ignore_signals=False,
           strip=False,
           upx=True,
-          console=True )
+          console=False )
 coll = COLLECT(exe,
                a.binaries,
                a.zipfiles,
@@ -34,4 +34,4 @@ coll = COLLECT(exe,
                strip=False,
                upx=True,
                upx_exclude=[],
-               name='poprecorder')
+               name='PopRecorder')
